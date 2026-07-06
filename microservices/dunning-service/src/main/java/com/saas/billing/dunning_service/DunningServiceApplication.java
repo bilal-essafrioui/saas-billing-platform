c2196f330dcf46590ffa9fbe8738835054c2a557
bilal-essafrioui <bilal.essafrioui@gmail.com>
2026-07-06 23:14:46 +0100

    feat: initial project structure

---
+++ microservices/dunning-service/src/main/java/com/saas/billing/dunning_service/DunningServiceApplication.java
@@ -0,0 +1,13 @@
+package com.saas.billing.dunning_service;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DunningServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DunningServiceApplication.class, args);
+	}
+
+}
---
+++ microservices/dunning-service/src/test/java/com/saas/billing/dunning_service/DunningServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.saas.billing.dunning_service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DunningServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
